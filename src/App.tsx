2f012a081d4bbef15db5a73e987263adfa32f99a
Walefe <walefedev@gmail.com>
2020-06-04 09:04:12 -0300

    Chore: create authenticate context.

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import SignIn from './pages/SignIn';
 
 import GlobalStyle from './styles/global';
 
+import AuthContext from './context/AuthContext';
+
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <AuthContext.Provider value={{ name: 'walefe' }}>
+      <SignIn />
+    </AuthContext.Provider>
     <GlobalStyle />
   </>
 );
